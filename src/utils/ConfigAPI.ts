5b9d341d3c21739bcb25d5dcd598ae82ee38f439
TThuyet <124869995+TThuyet@users.noreply.github.com>
2024-03-11 01:35:13 +0700

    url

--- src/utils/ConfigAPI.ts
+++ src/utils/ConfigAPI.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const request = axios.create({
-  baseURL: "https://85sjtr-8080.csb.app/",
+  baseURL: "http://localhost:3000/",
 });
 
 request.interceptors.request.use(
